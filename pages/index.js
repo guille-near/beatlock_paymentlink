5ffbbccc381cc7bdf02ac38a21cc408c698c9171
guille <94489066+guille-near@users.noreply.github.com>
2024-09-01 12:30:10 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
+import BeatstarsStripeIntegration from '../components/BeatstarsStripeIntegration';
 
+export default function Home() {
+  return (
+    <div>
+      <h1>Beatstars Stripe Integration</h1>
+      <BeatstarsStripeIntegration />
+    </div>
+  );
+}
